3ca6fc6be087ee06ad269b84f504b039be9f50c6
Andreas Rogne <andreas.rogne@softronic.se>
2025-03-20 13:02:28 +0100

    changed port frontend

--- dndapplication.client/vite.config.ts
+++ dndapplication.client/vite.config.ts
@@ -39,7 +39,7 @@ if (!fs.existsSync(certFilePath) || !fs.existsSync(keyFilePath)) {
 }
 
 const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}` :
-    env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'https://localhost:5001/api';
+    env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'https://localhost:8080/api';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -61,7 +61,7 @@ export default defineConfig({
                 secure: false
             }
         },
-        port: 5001,
+        port: 8080,
         https: {
             key: fs.readFileSync(keyFilePath),
             cert: fs.readFileSync(certFilePath),
